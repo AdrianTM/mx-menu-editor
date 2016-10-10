d9f323cd6192610787804ced56406a039f9426b9
Adrian <adrian@mxlinux.org>
2016-10-10 13:44:14 -0400

    changed URLs

---
+++ mx-menu-editor_16.10mx16.dsc
@@ -0,0 +1,32 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA1
+
+Format: 3.0 (native)
+Source: mx-menu-editor
+Binary: mx-menu-editor
+Architecture: any
+Version: 16.10mx16
+Maintainer: Steven Pusser (Stevo) <maintainer@mepiscommunity.org>
+Standards-Version: 3.9.5
+Vcs-Git: git://github.com/AdrianTM/mx-menu-editor
+Build-Depends: qt5-qmake, qt5-default, qttools5-dev-tools, debhelper (>= 7.0.50~)
+Package-List:
+ mx-menu-editor deb admin optional arch=any
+Checksums-Sha1:
+ 9f48a2450da83d7e668625d6e162a96c831cb80e 39852 mx-menu-editor_16.10mx16.tar.xz
+Checksums-Sha256:
+ d68cf9f3b8b5550b4df2636446f024864752846a8461f97eaba5efdcf0869360 39852 mx-menu-editor_16.10mx16.tar.xz
+Files:
+ 5c51ba43a5d80c38e8254c898b0e98e2 39852 mx-menu-editor_16.10mx16.tar.xz
+
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v1
+
+iQEcBAEBAgAGBQJX+9MbAAoJEHCTjHgGee6YCZsIAM26mtodcKJ1We/7ViQYDXo5
+w891ve4nVIQYRVSvyBa1qrz8YP3+h/zunfZec5Td1UnWiJdf8uLv7pVVd8FbQPWN
+0V3/tYuDZbpZMLnIw/CUXdlOmxd0DFN84c+DILPTPUXKK1aWuGVYucr+oFJv5K8B
+eOZRTnD1mVrcVXwJ69ySC4ier/zhvrZabgWtEVhKxoAanltlqB4AGNHbiyOlZy6X
+Ss8Oyt6Nk93kcjFyXu7VI2x7S0tKbEBijDXuMK6gQKsWg0mwy1D8RZ/fN2W5XT2I
+SuNBNzyK1nWEnkruLKROKYop9qAJQSLkEm7ZdLXhQYd7WYgCa+LcTc5duHl93mQ=
+=TfHg
+-----END PGP SIGNATURE-----
--- mxmenueditor.cpp
+++ mxmenueditor.cpp
@@ -4,7 +4,7 @@
  * Copyright (C) 2015 MX Authors
  *
  * Authors: Adrian
- *          MX & MEPIS Community <http://forum.mepiscommunity.org>
+ *          MX Linux <http://mxlinux.org>
  *
  * This file is part of MX Menu Editor.
  *
@@ -826,7 +826,7 @@ void mxmenueditor::on_buttonAbout_clicked()
                        tr("About MX Menu Editor"), "<p align=\"center\"><b><h2>" +
                        tr("MX Menu Editor") + "</h2></b></p><p align=\"center\">" + tr("Version: ") + version + "</p><p align=\"center\"><h3>" +
                        tr("Program for editing Xfce menu") +
-                       "</h3></p><p align=\"center\"><a href=\"http://www.mepiscommunity.org/mx\">http://www.mepiscommunity.org/mx</a><br /></p><p align=\"center\">" +
+                       "</h3></p><p align=\"center\"><a href=\"http://mxlinux.org\">http://mxlinux.org</a><br /></p><p align=\"center\">" +
                        tr("Copyright (c) MX Linux") + "<br /><br /></p>", 0, this);
     msgBox.addButton(tr("License"), QMessageBox::AcceptRole);
     msgBox.addButton(tr("Cancel"), QMessageBox::RejectRole);
@@ -844,7 +844,7 @@ void mxmenueditor::setEnabled(QString)
 // Help button clicked
 void mxmenueditor::on_buttonHelp_clicked()
 {
-    system("xdg-open http://mepiscommunity.org/wiki/help-files/help-mx-menu-editor");
+    system("xdg-open https://mxlinux.org/wiki/help-files/help-mx-menu-editor");
 }
 
 // Cancel button clicked
