5ba1fab8812a77e9327c62e84d5cebf5bf1967fc
oldgizahub <geoffgigg@hotmail.com>
2017-05-31 11:06:26 -0400

    revised translations for mx-menueditor: sk, .desktop, .pro

---
+++ translations/mx-menu-editor_sk.ts
@@ -0,0 +1,362 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sk" version="2.1">
+<context>
+    <name>AddAppDialog</name>
+    <message>
+        <location filename="../addappdialog.ui" line="20"/>
+        <source>Dialog</source>
+        <translation>Dialóg</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="26"/>
+        <source>Categories</source>
+        <translation>Kategórie</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="38"/>
+        <source>Add category</source>
+        <translation>Pridať kategóriu</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="51"/>
+        <source>Delete category</source>
+        <translation>Zmazať kategóriu</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="67"/>
+        <source>Command:</source>
+        <translation>Príkaz:</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="112"/>
+        <source>Save Changes</source>
+        <translation>Uložiť zmeny</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="120"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="158"/>
+        <source>Quit dialog</source>
+        <translation>Ukončiť dialóg</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="161"/>
+        <location filename="../addappdialog.cpp" line="102"/>
+        <source>Cancel</source>
+        <translation>Zrušiť</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="169"/>
+        <source>Alt+N</source>
+        <translation>Alt+N</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="184"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="191"/>
+        <source>Name:</source>
+        <translation>Názov:</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="198"/>
+        <source>Comment:</source>
+        <translation>Komentár:</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="205"/>
+        <source>Icon:</source>
+        <translation>Ikona:</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="212"/>
+        <location filename="../addappdialog.cpp" line="121"/>
+        <source>Set icon</source>
+        <translation>Nastaviť ikonu</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="225"/>
+        <source>Run in Terminal</source>
+        <translation>Spustiť v Termináli</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.ui" line="238"/>
+        <source>Startup Notifier</source>
+        <translation>Ohlásenie pri štarte</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.cpp" line="37"/>
+        <source>Add Custom Application</source>
+        <translation>Pridať vlastnú aplikáciu</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.cpp" line="54"/>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.cpp" line="54"/>
+        <source>Could not save the file</source>
+        <translation>Súbor nejde uložiť</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.cpp" line="102"/>
+        <source>Save changes?</source>
+        <translation>Uložiť zmeny?</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.cpp" line="102"/>
+        <source>Do you want to save your edits?</source>
+        <translation>Chcete uložiť zmeny?</translation>
+    </message>
+    <message>
+        <location filename="../addappdialog.cpp" line="102"/>
+        <source>Save</source>
+        <translation>Uložiť</translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="../main.cpp" line="51"/>
+        <source>You must run this program as normal user.</source>
+        <translation>Tento program musíte spustiť ako normálny užívateľ.</translation>
+    </message>
+</context>
+<context>
+    <name>mxmenueditor</name>
+    <message>
+        <location filename="../mxmenueditor.ui" line="14"/>
+        <location filename="../mxmenueditor.cpp" line="834"/>
+        <source>MX Menu Editor</source>
+        <translation>MX Menu Editor</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="31"/>
+        <source>Advanced Editor</source>
+        <translation>Pokročilý Editor</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="52"/>
+        <source>Categories and applications</source>
+        <translation>Kategórie a aplikácie</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="79"/>
+        <source>Add custom application</source>
+        <translation>Pridať vlastnú aplikáciu</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="92"/>
+        <source>Options</source>
+        <translation>Nastavenia</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="101"/>
+        <source>Run in terminal</source>
+        <translation>Spustiť v Termináli</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="111"/>
+        <source>Hide</source>
+        <translation>Skryť</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="121"/>
+        <source>Notify startup</source>
+        <translation>Ohlásiť pri štarte</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="144"/>
+        <source>Quick Editor</source>
+        <translation>Rýchly Editor</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="156"/>
+        <source>Comment:</source>
+        <translation>Komentár:</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="166"/>
+        <source>Restore original item</source>
+        <translation>Obnoviť pôvodnú položku</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="176"/>
+        <source>Command:</source>
+        <translation>Príkaz:</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="186"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="200"/>
+        <source>Name:</source>
+        <translation>Názov:</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="221"/>
+        <source>Icon</source>
+        <translation>Ikona</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="261"/>
+        <location filename="../mxmenueditor.cpp" line="542"/>
+        <source>Change icon</source>
+        <translation>Zmeniť ikonu</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="274"/>
+        <source>Categories</source>
+        <translation>Kategórie</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="286"/>
+        <source>Add</source>
+        <translation>Pridať</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="299"/>
+        <source>Delete</source>
+        <translation>Zmazať</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="364"/>
+        <source>Quit application</source>
+        <translation>Zatvoriť aplikáciu</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="367"/>
+        <source>Close</source>
+        <translation>Zatvoriť</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="375"/>
+        <source>Alt+N</source>
+        <translation>Alt+N</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="413"/>
+        <source>Display help</source>
+        <translation>Zobraziť nápovedu</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="416"/>
+        <source>Help</source>
+        <translation>Pomocník</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="424"/>
+        <source>Alt+H</source>
+        <translation>Alt+H</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="443"/>
+        <source>Save Changes</source>
+        <translation>Uložiť zmeny</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="451"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="486"/>
+        <source>About this application</source>
+        <translation>O tejto aplikácii</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="489"/>
+        <source>About...</source>
+        <translation>O Programe</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.ui" line="497"/>
+        <source>Alt+B</source>
+        <translation>Alt+B</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="463"/>
+        <source>Select executable file</source>
+        <translation>Vyberte spustiteľný súbor</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="526"/>
+        <source>Image Files (*.png *.jpg *.bmp *.xpm)</source>
+        <translation>Súbory obrázkov (*.png *.jpg *.bmp *.xpm)</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="713"/>
+        <source>Choose category</source>
+        <translation>Vybrať kategóriu</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="724"/>
+        <location filename="../mxmenueditor.cpp" line="839"/>
+        <location filename="../mxmenueditor.cpp" line="870"/>
+        <source>Cancel</source>
+        <translation>Zrušiť</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="725"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="815"/>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="815"/>
+        <source>Could not save the file</source>
+        <translation>Súbor nejde uložiť</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="833"/>
+        <source>About MX Menu Editor</source>
+        <translation>O programe MX Menu Editor</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="834"/>
+        <source>Version: </source>
+        <translation>Verzia:</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="835"/>
+        <source>Program for editing Xfce menu</source>
+        <translation>Program na editáciu Xfce ponuky</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="837"/>
+        <source>Copyright (c) MX Linux</source>
+        <translation>Copyright (c) MX Linux</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="838"/>
+        <source>License</source>
+        <translation>Licencia</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="870"/>
+        <source>Save changes?</source>
+        <translation>Uložiť zmeny?</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="870"/>
+        <source>Do you want to save your edits?</source>
+        <translation>Chcete uložiť zmeny?</translation>
+    </message>
+    <message>
+        <location filename="../mxmenueditor.cpp" line="870"/>
+        <source>Save</source>
+        <translation>Uložiť</translation>
+    </message>
+</context>
+</TS>
